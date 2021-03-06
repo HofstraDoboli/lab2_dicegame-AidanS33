282e227fe057cc1843969cc6bc85062d964c9eff
aidans33 <aschreiner1@pride.hofstra.edu>
2021-03-06 20:46:40 +0000

    Finished code, only issue being the only way to end the code is with control c

--- play_dice.cpp
+++ play_dice.cpp
@@ -15,9 +15,10 @@ int main()
     // Add your code to play the dice game here
     char S = 'a'; 
     cout << "Welcome to the Dice Game! In this game you will roll 2 dice. If you roll a 7 or 11, you win. If you roll a 2,3,or 12, you lose. " << endl;
-    cout << "To play the game, type Y ";
+    cout << "To play the game, type Y " ;
     cin >> S;
-    while (S == 'Y' || 'y') {
+    while (S == 'Y' || 'y') 
+    {
         int dice1;
         dice1 = rand()%6+1;
         int dice2;
@@ -25,32 +26,32 @@ int main()
         int sum;
         sum = dice1 + dice2;
         cout << "You rolled a " << dice1 << " and a " << dice2 << " For a sum of " << sum << endl;
-        if (sum == 7 || 11) {
+        if (sum == 7 || sum == 11) {
             cout << "Congratulations! You Win!" << endl;
         }
-        else if (sum == 2 || 3 || 12) {
+        else if (sum == 2 || sum == 3 || sum == 12) {
             cout << "I'm Sorry, You Lost." << endl;
         }
         else {
             int point = sum;
             cout << "Your point is " << point << endl;
-            bool not_win = false;
-            bool not_loss = false;
-            while (not_win && not_loss == true) {
+            bool winner = false;
+            bool loser = false;
+            while (winner == false && loser == false) {
                 dice1 = rand()%6+1;
                 dice2 = rand()%6+1;
                 sum = dice1 + dice2;
-                cout << "You rolled a " << dice1 << "and a " << dice2 << "for a sum of " << sum << endl;
-                if (sum != point) {
-                    not_win = false;
+                cout << "You rolled a " << dice1 << " and a " << dice2 << " for a sum of " << sum << endl;
+                if (sum == point) {
+                    winner = true;
                 }
-                if (sum == 7){
-                    not_loss = false;
+                else if (sum == 7){
+                    loser = true;
                 }
-                if (not_win == true){
+                if (loser == true){
                     cout << "You lose. " << endl;
                 }
-                else{
+                else if (winner == true){
                     cout << "You win! " << endl;
                 }
             }
